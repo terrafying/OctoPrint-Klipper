a738f26349409d65dc9c455d4a8fc88e5cc6121c
seanauff <sean.auffinger@gmail.com>
2020-06-08 13:22:18 -0400

    add buildx script

---
+++ buildx.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+REPO=seanauff
+IMAGE=octoprint-klipper
+PLATFORMS="linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64"
+
+docker context use x86_64
+export DOCKER_CLI_EXPERIMENTAL="enabled"
+docker buildx use multiarch-builder
+
+# Build & push latest
+docker buildx build -t ${REPO}/${IMAGE}:latest --compress --push --platform "${PLATFORMS}" .
