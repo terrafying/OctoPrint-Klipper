44bbd2acc0ccc343022a9234f0f3ce723d454f30
seanauff <seanauff@users.noreply.github.com>
2020-07-08 09:37:18 -0400

    dont use cache

--- build_scripts/build-amd64.sh
+++ build_scripts/build-amd64.sh
@@ -5,4 +5,4 @@ IMAGE=octoprint-klipper
 TAG=amd64
 
 # Build & push latest
-docker build -t ${REPO}/${IMAGE}:${TAG} --compress ../
+docker build -t ${REPO}/${IMAGE}:${TAG} --pull --no-cache --compress ../
